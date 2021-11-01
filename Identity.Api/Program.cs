d666dfa571c99e15741810aa9121ab05d4e21e86
Kunlapath Paengsa <kunlapath@buzzebees.com>
2021-11-01 16:02:59 +0700

    Set Json Appsetting

--- Identity.Api/Program.cs
+++ Identity.Api/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,9 +19,23 @@ namespace Identity.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+               .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                    config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    config.AddJsonFile($"swaggersettings.{context.HostingEnvironment.EnvironmentName}.json");
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    //webBuilder.RegisterAzureVault();
                     webBuilder.UseStartup<Startup>();
+                }).ConfigureServices(config =>
+                {
+                    //config.AddHostedService<EFMigrateDatabaseService>();
+                    //config.AddHostedService<AzureTableMigrateService>();
+                   // config.AddHostedService<OpenIddictDefaultDataWorkerService>();
                 });
     }
 }
